cbc7f986d69f96c05932affe702ebbc910653887
Craig Eales <craig.eales@gmail.com>
2013-07-09 22:18:23 +0100

    Document TriBoolean Constructor and canEqual

--- src/main/scala/atb/tribool/TriBoolean.scala
+++ src/main/scala/atb/tribool/TriBoolean.scala
@@ -7,8 +7,13 @@
 package atb
 package tribool {
 
-  /**
-    * See [[atb.tribool.TriBoolean.Negatable]]
+  /** Abstract class representing a TriBool with behaviour
+    *
+    * @constructor create a TriBoolean that represents a value
+    * with assoicated operations
+    *
+    * @param value TriBoolRepresentation that is the base value
+    * that is having meaning added to.
     */
 
   abstract class TriBoolean(val value: TriBoolRepresentation) {
@@ -21,6 +26,13 @@ package tribool {
         case _ =>
           false
       }
+
+    /** canEqual is a helper method to allow the correct semantics of
+     * equals to be implemented in the hierarchy
+     *
+     * @see "Programming In Scala: A Comprehnsive Step-By-Step Guide"
+     * (Stairway book) chapter 30 - Object Equality
+     */
 
     def canEqual(other: Any): Boolean =
       other.isInstanceOf[TriBoolean]
