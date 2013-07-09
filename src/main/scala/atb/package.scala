45faa4a215e41caaea293ee03c7f466befa95351
Craig Eales <craig.eales@gmail.com>
2013-07-09 21:58:28 +0100

    Document root atb package
    Add a basic documentation set for the root package
    Right now just adds a section for subpackages and
    references and adds a simple element for each

--- src/main/scala/atb/package.scala
+++ src/main/scala/atb/package.scala
@@ -5,7 +5,23 @@
 //
 
 /**
-  *  Some documetnation for atb
+  * =scala-atb (Scala All-Things-Boolean)=
+  *
+  * A collection of types and algorithms related to things Boolean
+  *
+  * ==Subpackages==
+  *
+  *  1. [[atb.tribool]] A package containing basic type for
+  *      three valued logic. In particular implementations of:
+  *      - Kleene 3-valued logic [[atb.tribool.KleeneTriBoolean]]
+  *      - Lukasiewicz logic [[atb.tribool.LukasiewiczTriBoolean]]
+  *
+  * ==References==
+  *
+  *  1. Three Valued Logic
+  *    a. [[http://en.wikipedia.org/wiki/Tribool]]
+  *
   */
+
 package object atb {
 }
