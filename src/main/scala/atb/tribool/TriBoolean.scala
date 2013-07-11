8099f3e3221b58b5a55ba120cf90cf94d5b46150
Craig Eales <craig.eales@gmail.com>
2013-07-11 21:09:57 +0100

    Update TriBoolean.scala documentation

--- src/main/scala/atb/tribool/TriBoolean.scala
+++ src/main/scala/atb/tribool/TriBoolean.scala
@@ -7,6 +7,16 @@
 package atb
 package tribool {
 
+  /**
+    * A TriBoolean is a class with three distinguished values
+    *  - True
+    *  - False
+    *  - Unknown
+    *
+    * @tparam A The implementing type of the values. Note `A` needs
+    * to be subclass of `TriBoolean[A]`.
+    */
+
   abstract class TriBoolean[A] {
     this: A =>
 
@@ -19,11 +29,10 @@ package tribool {
     * Negatable captures the notion of a [[TriBoolean]] that
     * can be negated, both with the property `not` and unary `!`
     *
-    * @tparam A the name of the implementing [[TriBoolean]] Class
+    * @tparam A the implementing [[TriBoolean]] Class
     */
 
   trait Negatable[A <: TriBoolean[A]] {
-    this: A =>
     def not: A
     def unary_! = not
   }
@@ -32,11 +41,10 @@ package tribool {
     * Conjunctable captures the notion of a [[TriBoolean]] that
     * can be conjoined, with the binary operator `and`
     *
-    * @tparam A the name of the implementing [[TriBoolean]] Class
+    * @tparam A the implementing [[TriBoolean]] Class
     */
 
   trait Conjunctable[A <: TriBoolean[A]] {
-    this: A =>
     def and(other: A): A
   }
 
@@ -44,11 +52,10 @@ package tribool {
     * Disjunctable captures the notion of a [[TriBoolean]] that
     * can be disjoined, with the binary operator `or`
     *
-    * @tparam A the name of the implementing [[TriBoolean]] Class
+    * @tparam A the implementing [[TriBoolean]] Class
     */
 
   trait Disjunctable[A <: TriBoolean[A]] {
-    this: A =>
     def or(other: A): A
   }
 
@@ -57,20 +64,25 @@ package tribool {
     * can be used in an implication,
     * via the binary operator `implies`
     *
-    * @tparam A the name of the implementing [[TriBoolean]] Class
+    * @tparam A the implementing [[TriBoolean]] Class
     */
 
   trait Implicable[A <: TriBoolean[A]] {
-    this: A =>
     def implies(other: A): A
   }
 
+  /**
+    * AnalogyImplication, the implementation of implies
+    * Carried over from the two-value logic.
+    *
+    * `a implies b === !a or b`
+    */
+
   trait AnalogyImplication[
     A <: TriBoolean[A]
-        with Disjunctable[A]
-        with Negatable[A]] {
+        with Disjunctable[A]] {
 
-    this: A =>
+    this: Negatable[A] =>
 
     def implies(other: A): A = !this or other
   }
